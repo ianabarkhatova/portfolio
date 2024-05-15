86dc41207ef328d0d84a8d8248be2fcd5f9f7342
Iana Barkhatova <yanabarhat@gmail.com>
2024-05-15 15:47:45 +0300

    Fix Header menu color

--- src/layout/header/Header_Styles.ts
+++ src/layout/header/Header_Styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Header = styled.header`
-    background-color: #fae2e5;
+    background-color: rgba(250, 226, 229, 0.2);
     padding: 20px 0;
     position: fixed;
     top: 0;
